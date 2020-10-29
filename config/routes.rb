9f903929471f16e4d79b53d40b1a36ade65f4a31
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-29 14:49:33 -0400

    Create friendship relationship. Create Friendship controller. Update the views to follow friend, stop following friend. Update routes to include the my friend route and the search friend route and implemente the search friend method in the user model

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_crypto', to: 'cryptos#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
